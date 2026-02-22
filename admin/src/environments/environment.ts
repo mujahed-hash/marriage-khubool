257ab3085bc0b82d91669408d3c5957945644d3a
Mohammed Mujahed Ahmed <mujahedahmed786@gmail.com>
2026-02-22 17:30:16 -0500

    Adjust admin budgets and relative URLs for production

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5006/api',
-    backendUrl: 'http://localhost:5006'
+    production: true,
+    apiUrl: '/api',
+    backendUrl: ''
 };
